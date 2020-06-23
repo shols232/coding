a214f65c98ee64918ef77d9997cdd89336396e10
shols232 <akinsolaademolatemitope@gmail.com>
2020-06-23 04:36:21 +0100

    'added more CRUD functionalities'

--- coding_challenge/urls.py
+++ coding_challenge/urls.py
@@ -6,9 +6,10 @@ from . import views
 urlpatterns = [
     path('users/<int:task_id>/', views.user_list_view, name='users'),
     path('login/', views.log_in, name="login"),
+    path('logout/', views.logout_view, name="logout"),
     path("", views.home, name="home"),
     path('tasks/', views.task_list_view, name='tasks'),
-    path('users/<int:request_user_id>/<int:task_id>/<int:user_id>/', views.user_send_request_view, name='user_send_request'),
+    path('users/<int:user_id>/<int:task_id>/', views.user_send_request_view, name='user_send_request'),
     path('notifications/', views.notifications_view, name='notifications'),
     path('challenges/', views.challenges_view, name='challenges'),
     path('notification/process/<int:user_id>/<int:task_id>/<str:notification_status>/', views.notifications_processing_view, name='user_process_request')
--- coding_challenge/views.py
+++ coding_challenge/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 import json
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
@@ -9,9 +9,8 @@ from django.conf import settings
 
 static = settings.STATIC_URL
 
-
+@login_required
 def home(request):
-    print(request.user.id)
     return render(request, 'home.html')
 
 
@@ -25,28 +24,11 @@ def log_in(request):
                     if username == user['username'] and password == user['password']:
                         user = User.objects.create_user(id=user['id'], username=user['username'], password=user['password'])
                         login(request, user)
-                        global logged_in_user
-                        logged_in_user = user
                         return redirect("home")
                 else:
                     return render(request, "login.html", {'error': 'Invalid credentials'})
-        try:
-            if logged_in_user:
-                with open('static/users.json') as data_file:
-                    users = json.load(data_file)
-                    global updated_users_list
-                    updated_users_list = [logged_in_user if user['id'] == logged_in_user['id'] else user for user in users ]
-                    for user in users:
-                        if user['id'] == int(logged_in_user['id']):
-                            return redirect("home", {'user_id':logged_in_user['id']})
-
-                with open('static/users.json', 'w') as data_file:
-                    json.dump(updated_users_list, data_file)
-        except NameError:
-            pass
-
         return render(request, "login.html")
-
+@login_required
 def user_list_view(request, task_id):
 
     with open('static/users.json') as data_file:
@@ -56,23 +38,22 @@ def user_list_view(request, task_id):
         for user in users:
             if user["id"] != int(request.user.id):
                 challenging_users.append(user)
-        print(challenging_users[:3])
-        return render(request, 'users_list_page.html', {'users': challenging_users, 'requesting_user_id': request.user.id, 'task_id': task_id})
-
+        return render(request, 'users_list_page.html', {'users': challenging_users, 'task_id': task_id})
+@login_required
 def task_list_view(request):
 
     with open('static/tasks.json') as data_file:
         tasks = json.load(data_file)
         return render(request, 'task_list.html', {'tasks': tasks})
 
-def user_send_request_view(request, request_user_id, task_id, user_id):
-    request_user_id = request_user_id
+@login_required
+def user_send_request_view(request, user_id, task_id):
+    request_user_id = request.user.id
     task_id = task_id
     user_id = user_id
     notification = {}
 
     requesting_user = {}
-    print(user_id, task_id, request_user_id)
     with open('static/users.json') as data_file:
         first_users = json.load(data_file)
         for user in first_users:
@@ -80,20 +61,17 @@ def user_send_request_view(request, request_user_id, task_id, user_id):
                 requesting_user = user
             if user['id'] == int(user_id):
                 user = user
-                print(user)
 
         for notification in user["notifications"]:
             if notification['request_user_id'] == request_user_id and notification['task_id'] == task_id:
-                print('hello1')
                 return render(request, 'request_sent.html', {"sent": True})
 
     with open('static/tasks.json') as data_file:
         tasks = json.load(data_file)
         for task in tasks:
             if task['id'] == int(task_id):
-                print('holla')
                 notification['status'] = 'pending'
-                notification['message'] = f'Hello {user["username"]}, {requesting_user["username"]} has challenged you to a duel in {task["task"]}'
+                notification['message'] = f'{requesting_user["username"]} has challenged you to a duel in {task["task"]}'
                 # notification['task'] = f'task: {user["task"]}'
                 notification['task_id'] = int(task_id)
                 notification['request_user_id'] = int(request_user_id)
@@ -110,11 +88,10 @@ def user_send_request_view(request, request_user_id, task_id, user_id):
             with open('static/users.json', 'w') as json_file:
                 json.dump(new_user_list, json_file)
 
-    print('hello 2')
     return render(request, 'request_sent.html', {"sent": False})
 
+@login_required
 def notifications_view(request):
-    print(request.user)
     with open('static/users.json') as data_file:
         users = json.load(data_file)
         for user in users:
@@ -122,25 +99,34 @@ def notifications_view(request):
                 user = user
                 return render(request, 'notification.html', {'user': user, 'notifications': user['notifications']})
 
-def notifications_processing_view(request, user_id, task_id, notification_status):
-    current_user = None
+@login_required
+def notifications_processing_view(request,user_id, task_id, notification_status):
     with open('static/users.json') as data_file:
         users = json.load(data_file)
         new_users_list = []
         for user in users:
-            if user['id'] == int(user_id):
-                current_user = user
+            if user['id'] == int(request.user.id) or user['id'] == user_id:
                 if notification_status == 'accept':
+                    new_notifications = [notification for notification in user['notifications'] if
+                                         notification['task_id'] != task_id]
                     with open('static/tasks.json') as file:
                         tasks = json.load(file)
+                        user['notifications'] = new_notifications
                         for task in tasks:
                             if task['id'] == task_id:
                                 new_task = task
                                 if new_task in user['active']:
-                                    pass
+                                    for user_ in users:
+                                        if user_['id'] == user_id:
+                                            new_users_list.append(user_)
+                                    new_users_list.append(user)
                                 else:
                                     user['active'].append(new_task)
-                                new_users_list.append(user)
+                                    for user_ in users:
+                                        if user_['id'] == user_id:
+                                            user_['active'].append(new_task)
+                                            new_users_list.append(user_)
+                                    new_users_list.append(user)
                 elif notification_status == 'decline':
                     new_notifications = [notification for notification in user['notifications'] if notification['task_id'] != task_id ]
                     user['notifications'] = new_notifications
@@ -148,14 +134,18 @@ def notifications_processing_view(request, user_id, task_id, notification_status
             else:
                 new_users_list.append(user)
 
-    print(new_users_list)
-    with open('static/users.json', 'w') as json_file:
-        json.dump(new_users_list, json_file)
-    if current_user:
-        return render(request, 'challenges.html', {'user': current_user, 'tasks': current_user['active']})
-    else:
-        return render(request, 'error.html')
+        with open('static/users.json', 'w') as json_file:
+            json.dump(new_users_list, json_file)
 
+        with open('static/users.json') as json_file:
+            users = json.load(json_file)
+            for user in users:
+                if user['id'] == request.user.id:
+                    return render(request, 'challenges.html')
+            else:
+                return render(request, 'error.html')
+
+@login_required
 def challenges_view(request):
     with open('static/users.json') as data_file:
         users = json.load(data_file)
@@ -163,4 +153,8 @@ def challenges_view(request):
             if user['id'] == int(request.user.id):
                 user = user
                 return render(request, 'challenges.html', {'user': user, 'tasks': user['active']})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
 
