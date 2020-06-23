c96b7b6c1e2af3e126c3f8eec6bb35b0daec0bce
damiso15 <damiso15@yahoo.com>
2020-06-23 10:10:56 +0100

    initial commit

--- coding/settings.py
+++ coding/settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'staticfiles/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles/static/'),
 )
 
 LOGIN_URL = "/login"
