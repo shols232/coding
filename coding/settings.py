b1678325358de25f4dab3a5a6f31683e7921fb30
damiso15 <damiso15@yahoo.com>
2020-06-23 11:03:29 +0100

    initial commit

--- coding/settings.py
+++ coding/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['**.herokuapp.com', '127.0.0.1']
 
@@ -132,4 +132,4 @@ LOGIN_URL = "/login"
 
 django_heroku.settings(locals())
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
